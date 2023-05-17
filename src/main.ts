bf2ca49d3bba3427f4dd40e71de2c77021e99683
Sean <770899447@qq.com>
2023-05-17 08:03:55 +0800

    doc文档配置

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 declare const module: any;
 import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
 import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { generateDocument } from './doc';
 
 async function bootstrap() {
-   const app = await NestFactory.create<NestFastifyApplication>(
+  const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
@@ -18,10 +22,7 @@ async function bootstrap() {
   });
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
+  generateDocument(app);
   await app.listen(3000);
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close())
-  }
 }
 bootstrap();
