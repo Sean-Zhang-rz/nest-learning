1f5f8098d12c0ac9534c880ac9ef42f0d81bcfb8
zhangrixin <770899447@qq.com>
2023-05-26 11:01:49 +0800

    guard, exception应用

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,29 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Inject,
+  UseFilters,
+} from '@nestjs/common';
+import { AaaFilter } from './aaa.filter';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    // private readonly appService: AppService,
-    private readonly appService: AppService,
-    @Inject('person') private readonly person: {name: string, age: number},
-    @Inject('person2') private readonly person2: {name: string, desc: string}
-  ) {}
+  @Inject('person')
+  private readonly person: { name: string; age: number };
+  @Inject('person2')
+  private readonly person2: { name: string; desc: string };
+  constructor(private readonly appService: AppService) {}
 
   @Get()
+  @UseFilters(AaaFilter)
   getHello(): string {
     console.log(this.person);
     console.log(this.person2);
 
-    return this.appService.getHello();
+    // return this.appService.getHello();
+    throw new HttpException('xxx', HttpStatus.BAD_REQUEST);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,27 +3,36 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AaaModule } from './aaa/aaa.module';
 import { BbbModule } from './bbb/bbb.module';
+import { AaaGuard } from './aaa.guard';
+import { AaaInterceptor } from './aaa.interceptor';
 
 @Module({
   imports: [AaaModule, BbbModule],
   controllers: [AppController],
-  providers: [AppService, {
-    provide: 'person',
-    useValue: {
-      name: 'aaa',
-      age: 20
-    }
-  }, {
-    provide: 'person2',
-    useFactory() {
-      return {
+  providers: [
+    AppService,
+    AaaGuard,
+    AaaInterceptor,
+    {
+      provide: 'person',
+      useValue: {
         name: 'aaa',
-        desc: 'person2'
-      }
-    }
-  }, {
-    provide: 'person4',
-    useExisting: 'person2'
-  }],
+        age: 20,
+      },
+    },
+    {
+      provide: 'person2',
+      useFactory() {
+        return {
+          name: 'aaa',
+          desc: 'person2',
+        };
+      },
+    },
+    {
+      provide: 'person4',
+      useExisting: 'person2',
+    },
+  ],
 })
 export class AppModule {}
