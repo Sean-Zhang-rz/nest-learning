046f2e2937692f4fcec5400bf904e9a8a5eb706b
zhangrixin <770899447@qq.com>
2023-06-07 17:04:45 +0800

    /

---
+++ src/database/index.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'sean',
+  database: 'practice'
+});
+
+connection.query(
+  'SELECT * FROM customers WHERE name Like ?',
+  ['李%'],
+  function(err, results, fields) {
+    console.log(results);
+    console.log(fields.map(item => item.name)); 
+  }
+);
